366654ede7ca68d00ef112c39fa336efbb2d8a75
owenlu2024 <owenlu.sh@gmail.com>
2026-05-19 13:16:12 +0800

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://qmjjexapzknwecevxwp.supabase.co",
-  anonKey: "sb_publishable_NhchcUUSjNdP4H3fSF3iUQ_saClCt_C"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtamlqZXhhcHprbndlY2V2eHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNDMwODAsImV4cCI6MjA5NDcxOTA4MH0.kW_PPTmRcuLYjLUC1-UDXHWYNwJCwYbR-SIc_swPrhk"
 };
